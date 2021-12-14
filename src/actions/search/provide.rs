49efc1f5578f8191f2afb48b231253d6f98405d3
Leo Shen <i@szclsya.me>
2021-12-14 15:03:46 -0500

    search/provide: combine same package
    - also sort the result based on how many files are matched

--- src/actions/search/provide.rs
+++ src/actions/search/provide.rs
@@ -7,6 +7,7 @@ use flate2::read::GzDecoder;
 use rayon::prelude::*;
 use regex::Regex;
 use std::{
+    collections::HashMap,
     fs::File,
     io::{BufRead, BufReader},
     path::PathBuf,
@@ -26,7 +27,10 @@ pub fn show_provide_file(
 
     // Find a list of package names that provide the designated file
     debug!("Searching Contents databases...");
-    let pkgnames = package_name_provide_file(&content_dbs, filename)?;
+    let mut pkgnames = Vec::from_iter(package_name_provide_file(&content_dbs, filename)?);
+    // Sort based on number of matched paths
+    pkgnames.sort_by_key(|(_, paths)| paths.len());
+    pkgnames.reverse();
 
     // Create a Solver so we can get more info    let mut solver = Solver::new();
     debug!("Constructing package pool...");
@@ -36,16 +40,17 @@ pub fn show_provide_file(
     let pool = pool::source::create_pool(&dbs, &[])?;
 
     debug!("Generating detailed package info...");
-    for (pkgname, path) in pkgnames {
+    for (pkgname, paths) in pkgnames {
         if let Some(pkgs) = pool.get_pkgs_by_name(&pkgname) {
             // This is safe unless the pool is broken
             let latest_pkg_id = pkgs.get(0).unwrap();
             let latest_pkg = pool.get_pkg_by_id(*latest_pkg_id).unwrap();
+            let paths_str = paths.join(", ");
             // Prepare a PkgInfo
             let pkginfo = PkgInfo {
                 pkg: latest_pkg,
                 has_dbg_pkg: pool.has_dbg_pkg(*latest_pkg_id)?,
-                additional_info: Some(format!("Provide file {}", style(path).bold())),
+                additional_info: Some(format!("Provides: {}", style(paths_str).bold())),
             };
             pkginfo.show(machine_status)?;
         }
@@ -55,7 +60,7 @@ pub fn show_provide_file(
 }
 
 // Given a filename or path, find package names that provide such file
-pub fn package_name_provide_file(dbs: &[PathBuf], filename: &str) -> Result<Vec<(String, String)>> {
+pub fn package_name_provide_file(dbs: &[PathBuf], filename: &str) -> Result<HashMap<String, Vec<String>>> {
     // Construct regex based on deb Contents file format
     let regex = if filename.starts_with('/') {
         // Absolute path, strip "/" to match Contents file format
@@ -72,32 +77,39 @@ pub fn package_name_provide_file(dbs: &[PathBuf], filename: &str) -> Result<Vec<
         ))?
     };
 
-    let mut res = Vec::new();
+    let mut res: HashMap<String, Vec<String>> = HashMap::new();
     for db in dbs {
         let f = File::open(db)?;
         let f = GzDecoder::new(f);
         let bufreader = BufReader::new(f);
-        let mut pkgnames = bufreader
+        let pkgs: Vec<(String, String)> = bufreader
             .lines()
             .par_bridge()
             .filter_map(|line| match line {
                 Ok(l) => {
                     if regex.is_match(&l) {
                         let captures = regex.captures(&l).unwrap();
-                        Some((
-                            captures.name("pkgname").unwrap().as_str().to_owned(),
-                            captures.name("path").unwrap().as_str().to_owned(),
-                        ))
+                        let pkgname = captures.name("pkgname").unwrap().as_str().to_owned();
+                        let mut path = captures.name("path").unwrap().as_str().to_owned();
+                        // Add `/` to the front of path, because Contents file uses relative path
+                        path.insert(0, '/');
+                        Some((pkgname, path))
                     } else {
                         None
                     }
                 }
                 Err(_) => None,
             })
-            .collect();
-        res.append(&mut pkgnames);
+        .collect();
+
+        for (pkgname, path) in pkgs {
+            if let Some(list) = res.get_mut(&pkgname) {
+                list.push(path);
+            } else {
+                res.insert(pkgname, vec![path]);
+            }
+        }
     }
 
-    res.dedup();
     Ok(res)
 }
