75812fc609fa0dc822066bdeae69b74dc854416b
liushuyu <liushuyu011@gmail.com>
2021-12-14 22:21:50 -0700

    utils/debcontrol: use nom to parse version constraints

--- src/utils/debcontrol.rs
+++ src/utils/debcontrol.rs
@@ -1,16 +1,49 @@
 use crate::types::VersionRequirement;
 
 use anyhow::{format_err, Result};
-use lazy_static::lazy_static;
-use regex::Regex;
+use nom::{
+    bytes::complete::{take_while, take_while1},
+    character::{complete::alphanumeric1, complete::char, is_alphanumeric},
+    combinator::{opt, recognize},
+    sequence::{delimited, separated_pair},
+    sequence::{pair, preceded},
+    IResult,
+};
+
+// parser combinators
+fn parse_package_name(s: &[u8]) -> IResult<&[u8], &[u8]> {
+    recognize(pair(
+        alphanumeric1,
+        take_while(|c| is_alphanumeric(c) || c == b'+' || c == b'-' || c == b'.'),
+    ))(s)
+}
+
+fn parse_version_op(s: &[u8]) -> IResult<&[u8], &[u8]> {
+    take_while1(|c| c == b'>' || c == b'<' || c == b'=')(s)
+}
+
+fn parse_version(s: &[u8]) -> IResult<&[u8], &[u8]> {
+    take_while1(|c| {
+        is_alphanumeric(c) || c == b'+' || c == b'-' || c == b'.' || c == b'~' || c == b':'
+    })(s)
+}
+
+fn parse_version_expr(s: &[u8]) -> IResult<&[u8], &[u8]> {
+    recognize(separated_pair(parse_version_op, char(' '), parse_version))(s)
+}
+
+fn parse_relation_suffix(s: &[u8]) -> IResult<&[u8], &[u8]> {
+    preceded(
+        char(' '),
+        delimited(char('('), parse_version_expr, char(')')),
+    )(s)
+}
+
+fn parse_relational(s: &[u8]) -> IResult<&[u8], (&[u8], Option<&[u8]>)> {
+    pair(parse_package_name, opt(parse_relation_suffix))(s)
+}
 
 pub fn parse_pkg_list(s: &str) -> Result<Vec<(String, VersionRequirement)>> {
-    lazy_static! {
-        static ref PKG_PARTITION: Regex = Regex::new(
-            r"^(?P<name>[A-Za-z0-9-.+]+)( \((?P<ver_req>[<>=]+ ?[A-Za-z0-9.\-:+~]+)\))?$"
-        )
-        .unwrap();
-    }
     if s.is_empty() {
         return Ok(Vec::new());
     }
@@ -18,18 +51,43 @@ pub fn parse_pkg_list(s: &str) -> Result<Vec<(String, VersionRequirement)>> {
     let mut res = Vec::new();
     let pkgs: Vec<&str> = s.split(", ").collect();
     for pkg in pkgs {
-        let segments = PKG_PARTITION
-            .captures(pkg)
-            .ok_or_else(|| format_err!("Malformed version in depends/breaks: {}", pkg))?;
+        let (_, (name, version)) = parse_relational(pkg.as_bytes())
+            .map_err(|_| format_err!("Malformed version in depends/breaks: {}", pkg))?;
         // The regex should ensure name always exist
-        let name = segments.name("name").unwrap().as_str().to_string();
-        let ver_req = match segments.name("ver_req") {
-            Some(s) => VersionRequirement::try_from(s.as_str())?,
+        let ver_req = match version {
+            Some(s) => VersionRequirement::try_from(std::str::from_utf8(s)?)?,
             None => VersionRequirement::new(),
         };
         // Add to result
-        res.push((name, ver_req));
+        res.push((std::str::from_utf8(name)?.to_string(), ver_req));
     }
 
     Ok(res)
+}
+
+#[test]
+fn test_parsers() {
+    assert_eq!(parse_version_op(&b">>"[..]), Ok((&b""[..], &b">>"[..])));
+    assert_eq!(parse_version_op(&b"<="[..]), Ok((&b""[..], &b"<="[..])));
+    assert_eq!(
+        parse_version_expr(&b">= 2:1.1.0~rc.1"[..]),
+        Ok((&b""[..], &b">= 2:1.1.0~rc.1"[..]))
+    );
+    assert_eq!(
+        parse_package_name(&b"sqlite-ass"[..]),
+        Ok((&b""[..], &b"sqlite-ass"[..]))
+    );
+    assert_eq!(
+        parse_package_name(&b"sqlite_ass"[..]),
+        Ok((&b"_ass"[..], &b"sqlite"[..]))
+    );
+    assert_eq!(
+        parse_relational(&b"libpcap (>= 1.9.1)"[..]),
+        Ok((&b""[..], (&b"libpcap"[..], Some(&b">= 1.9.1"[..]))))
+    );
+    assert_eq!(
+        parse_relational(&b"libpcap"[..]),
+        Ok((&b""[..], (&b"libpcap"[..], None)))
+    );
+    assert!(parse_relational(&b"libpcap_invalid (>= 1.9.1)"[..]).is_err());
 }
