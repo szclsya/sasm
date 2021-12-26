547506d84c9179f4618c583365bf1e9a1346c042
liushuyu <liushuyu011@gmail.com>
2021-12-26 02:34:36 -0700

    actions/search: add SSE2 support ...
    ... slower, but support x86 CPUs back from 2001

--- src/actions/search/matcher/mod.rs
+++ src/actions/search/matcher/mod.rs
@@ -33,6 +33,52 @@ fn search_rk_generic(haystack: &[u8], needle: &[u8]) -> Option<usize> {
     None
 }
 
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+#[target_feature(enable = "sse2")]
+unsafe fn search_sse2(haystack: &[u8], needle: &[u8]) -> Option<usize> {
+    use std::arch::x86_64::*;
+
+    if needle.len() < 5 {
+        return search_simple(haystack, needle);
+    }
+
+    let mut h_ptr = haystack.as_ptr() as *const u8;
+    let h_end = /* unsafe */ { h_ptr.add(haystack.len() - needle.len()) };
+    /* unsafe */
+    {
+        let needle_4b: *const i32 = std::mem::transmute(needle.as_ptr());
+        let n_prefix = _mm_set1_epi32(*needle_4b);
+
+        while h_ptr.add(16) <= h_end {
+            let h0 = _mm_loadu_si128(h_ptr as *const _);
+            let mask0 = _mm_movemask_epi8(_mm_cmpeq_epi32(h0, n_prefix));
+            let h1 = _mm_loadu_si128(h_ptr.offset(1) as *const _);
+            let mask1 = _mm_movemask_epi8(_mm_cmpeq_epi32(h1, n_prefix));
+            let h2 = _mm_loadu_si128(h_ptr.offset(2) as *const _);
+            let mask2 = _mm_movemask_epi8(_mm_cmpeq_epi32(h2, n_prefix));
+            let h3 = _mm_loadu_si128(h_ptr.offset(3) as *const _);
+            let mask3 = _mm_movemask_epi8(_mm_cmpeq_epi32(h3, n_prefix));
+
+            if (mask0 | mask1 | mask2 | mask3) != 0 {
+                let current = std::slice::from_raw_parts(h_ptr, 16 + needle.len());
+                for i in 0..16 {
+                    if current[i..(i + needle.len())] == *needle {
+                        let rest_offset = h_ptr.offset_from(haystack.as_ptr()) as usize;
+                        return Some(i + rest_offset);
+                    }
+                }
+            }
+
+            h_ptr = h_ptr.add(16);
+        }
+
+        // last < 19 bytes
+        let rest_offset = h_ptr.offset_from(haystack.as_ptr()) as usize;
+        let rest = search_rk_generic(&haystack[rest_offset..], needle);
+        return rest.and_then(|x| Some(rest_offset + x));
+    }
+}
+
 /// Adapted from https://github.com/ashvardanian/CppBenchSubstrSearch/blob/444d9acc627fa23ed2f21401ffa45d2b919a66a8/substr_search.hpp
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 #[target_feature(enable = "avx2")]
@@ -83,6 +129,8 @@ pub fn search_rk_fast(haystack: &[u8], needle: &[u8]) -> Option<usize> {
     {
         if is_x86_feature_detected!("avx2") {
             return unsafe { search_avx2(haystack, needle) };
+        } else if is_x86_feature_detected!("sse2") {
+            return unsafe { search_sse2(haystack, needle) };
         }
     }
     return search_rk_generic(haystack, needle);
