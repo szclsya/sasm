a06dd579d952955933265af730ebe7ecf44df025
Leo Shen <i@szclsya.me>
2022-01-15 05:10:54 -0500

    feat(config): support tags
    - read by other programs

--- src/types/config/mod.rs
+++ src/types/config/mod.rs
@@ -42,6 +42,7 @@ pub struct UnsafeConfig {
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct RepoConfig {
     pub source: Mirror,
+    pub tags: Option<Vec<String>>,
     pub distribution: String,
     pub components: Vec<String>,
     pub keys: Vec<String>,
