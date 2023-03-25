45c872813bde5644e4ba62fb71d1018127d9d2dd
Leo Shen <i@szclsya.me>
2023-03-25 18:04:55 -0400

    fix: ignore malformed package desc files
    - rather than just crash

--- src/pool/source/pacdb.rs
+++ src/pool/source/pacdb.rs
@@ -1,6 +1,6 @@
 /// The pacman db reader
 use crate::{
-    debug, error,
+    debug, warn,
     pool::PkgPool,
     types::{Checksum, PkgMeta, PkgSource, PkgVersion, VersionRequirement},
     utils::{downloader, pacparse},
@@ -19,21 +19,28 @@ pub fn import(db: &Path, pool: &mut dyn PkgPool, baseurl: &str) -> Result<()> {
     let mut tar = Archive::new(gzipdecoder);
 
     for file in tar.entries()? {
-        let mut file = file.context("error reading file from db")?;
+        let file = file.context("error reading file from db")?;
         let path = file.path()?.to_path_buf();
+        let path_str = path.display().to_string();
         if path.ends_with("desc") {
             // Now we are talking!
-            let mut content = String::new();
-            file.read_to_string(&mut content)
-                .context("error reading desc file from db")?;
-            let fields = pacparse::parse_str(&content)
-                .context(format!("error parsing desc {}", path.display()))?;
-            let pkgmeta = fields_to_pkgmeta(fields)
-                .context(format!("error reading fields in {}", path.display()))?;
-            pool.add(pkgmeta);
+            match parse_desc(file, &path_str) {
+                Ok(pkgmeta) => { pool.add(pkgmeta); },
+                Err(e) => { warn!("Failed to add {path_str} from {0}: {e}", db.display()); },
+            };
         }
     }
     Ok(())
+}
+
+fn parse_desc(mut f: impl Read, from: &str) -> Result<PkgMeta> {
+    let mut content = String::new();
+    f.read_to_string(&mut content).context("error reading desc file from db")?;
+    let fields = pacparse::parse_str(&content)
+                .context(format!("error parsing desc from {from}"))?;
+    let pkgmeta = fields_to_pkgmeta(fields)
+                .context(format!("error reading fields from {from}"))?;
+    Ok(pkgmeta)
 }
 
 fn fields_to_pkgmeta(mut f: HashMap<String, Vec<String>>) -> Result<PkgMeta> {
@@ -102,7 +109,7 @@ fn get_pkg_list(
             match pacparse::parse_package_requirement_line(&line) {
                 Ok((_, (name, verreq, desc))) => out.push((name.to_owned(), verreq, desc)),
                 Err(e) => {
-                    error!("bad package requirement when parsing {field_name}: {e}");
+                    warn!("bad package requirement when parsing {field_name}: {e}");
                     bail!("malformed package requirement for {pkgname} at line {i}");
                 }
             }
